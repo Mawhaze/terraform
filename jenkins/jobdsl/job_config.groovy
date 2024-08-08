728c6548bfbebfbb6fd8e2adc22c097d81519657
mawhaze <evancast92@gmail.com>
2024-08-07 18:09:09 -0600

    more testing

--- jenkins/jobdsl/job_config.groovy
+++ jenkins/jobdsl/job_config.groovy
@@ -46,9 +46,10 @@ pipeline {
                     'docker run -it --rm -e AWS_DEFAULT_REGION=us-west-2 \
                     -e AWS_ACCESS_KEY_ID=\$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=\$AWS_SECRET_ACCESS_KEY \
                     -e PROXMOX_USERNAME=\$PROXMOX_USERNAME -e PROXMOX_PASSWORD=\$PROXMOX_PASSWORD \
+                    -e TF_VAR_host_node=\$TF_VAR_host_node \
+                    -e TF_VAR_node_size=\$TF_VAR_node_size -e TF_VAR_vm_name=\$TF_VAR_vm_name \
                     mawhaze/terraform:latest \
-                    /bin/bash -c "terraform -chdir=./proxmox init && teraform --chdir=./proxmox plan -out= \
-                    terraform init && terraform plan -out=tfplan"'
+                    /bin/bash -c "terraform -chdir=./proxmox init && teraform --chdir=./proxmox plan -out=tfplan"'
                 )
             }
         }
@@ -64,6 +65,8 @@ pipeline {
                     'docker run -it --rm -e AWS_DEFAULT_REGION=us-west-2 \
                     -e AWS_ACCESS_KEY_ID=\$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=\$AWS_SECRET_ACCESS_KEY \
                     -e PROXMOX_USERNAME=\$PROXMOX_USERNAME -e PROXMOX_PASSWORD=\$PROXMOX_PASSWORD \
+                    -e TF_VAR_host_node=\$TF_VAR_host_node \
+                    -e TF_VAR_node_size=\$TF_VAR_node_size -e TF_VAR_vm_name=\$TF_VAR_vm_name \
                     mawhaze/terraform:latest \
                     /bin/bash -c "terraform -chdir=./proxmox apply tfplan"'
                 )
