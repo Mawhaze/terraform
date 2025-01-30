700be0bd6d57c716065b3dc9ac29e18bfdb43e66
mawhaze <evancast92@gmail.com>
2025-01-30 11:33:51 -0700

    removing k8s nodes, need to pull provider out of the child module

--- proxmox/modules/ubuntu_vm/providers.tf
+++ proxmox/modules/ubuntu_vm/providers.tf
@@ -1,8 +1,14 @@
-terraform {
-  required_providers {
-    proxmox = {
-      source = "telmate/proxmox"
-      version = "3.0.1-rc3"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     proxmox = {
+#       source = "telmate/proxmox"
+#       version = "3.0.1-rc3"
+#     }
+#   }
+# }
+
+# provider "proxmox" {
+#   pm_api_url = var.proxmox_api_url
+#   pm_user = var.proxmox_username
+#   pm_password = var.proxmox_password
+# }
